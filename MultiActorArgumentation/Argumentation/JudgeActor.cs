699291f94189da9ed981b493b33c66ac21915fed
DeathTost <deathtost@gmail.com>
2018-12-14 17:24:29 +0100

    Add argumentation actors

---
+++ MultiActorArgumentation/Argumentation/JudgeActor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiActorArgumentation.Argumentation
+{
+    public class JudgeActor : ReceiveActor
+    {
+        public JudgeActor()
+        {
+            Receive<string /*MESSAGE TYPE*/>((x) =>
+            {
+                //DO SOMETHING
+            });
+        }
+
+        protected override void PreStart()
+        {
+            //DO SOMETHING - MAYBE START ARGUMENTATION TREE 
+            base.PreStart();
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            //DO SOMETHING - WHAT TO DO IN CASE OF EXCEPTION
+            return base.SupervisorStrategy();
+        }
+    }
+}

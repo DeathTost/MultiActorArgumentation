f1a8b2bc07ea5f60c3e09fef21ae048d960d33e1
DeathTost <deathtost@gmail.com>
2018-12-16 12:20:52 +0100

    Add simple text processing

--- MultiActorArgumentation/Nlp/DocumentProcessorActor.cs
+++ MultiActorArgumentation/Nlp/DocumentProcessorActor.cs
@@ -6,6 +6,9 @@ using edu.stanford.nlp.tagger;
 using edu.stanford.nlp.parser.lexparser;
 using edu.stanford.nlp.process;
 using edu.stanford.nlp.trees;
+using System.Collections.Generic;
+using edu.stanford.nlp.pipeline;
+using edu.stanford.nlp.util;
 
 namespace MultiActorArgumentation.Nlp
 {
@@ -25,6 +28,12 @@ namespace MultiActorArgumentation.Nlp
                 var tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
                 var textReader = new StringReader(text);
                 var rawWords = tokenizerFactory.getTokenizer(textReader).tokenize();
+
+                var ngrams = GetNGramsPositions(rawWords, 1, 2);
+                foreach (var word in rawWords.toArray())
+                {
+                    System.Console.WriteLine(word);
+                }
                 textReader.close();
                 var tree = lp.apply(rawWords);
 
@@ -38,5 +47,51 @@ namespace MultiActorArgumentation.Nlp
                 tp.printTree(tree);
             });
         }
+
+        public List<java.util.List> GetNGramsPositions(java.util.List items, int minSize, int maxSize)
+        {
+            List<java.util.List> ngrams = new List<java.util.List>();
+            int listSize = items.size();
+            for (int i = 0; i < listSize; ++i)
+            {
+                for (int ngramSize = minSize; ngramSize <= maxSize; ++ngramSize)
+                {
+                    if (i + ngramSize <= listSize)
+                    {
+                        java.util.List ngram = new java.util.ArrayList();
+                        for (int j = i; j < i + ngramSize; ++j)
+                        {
+                            ngram.add(items.get(j).ToString());
+                        }
+                        ngram.add(i.ToString());
+                        ngrams.Add(ngram);
+                    }
+                }
+            }
+            return ngrams;
+        }
+
+        public void Tokenize()
+        {
+            var jarRoot = "...\\...\\...\\stanford-corenlp-full-2017-06-09";
+            var modelsDirectory = jarRoot + "\\edu\\stanford\\nlp\\models";
+            var curDir = System.Environment.CurrentDirectory;
+            System.IO.Directory.SetCurrentDirectory(jarRoot);
+
+            Properties properties = new Properties();
+            properties.setProperty("annotators", "tokenize,ssplit,pos,lemma");
+            StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
+            System.IO.Directory.SetCurrentDirectory(curDir);
+            Annotation tokenAnnotation = new Annotation("Disability is to be able to achieve the greatest in life");
+            pipeline.annotate(tokenAnnotation);
+
+            using (var stream = new ByteArrayOutputStream())
+            {
+                pipeline.prettyPrint(tokenAnnotation, new PrintWriter(stream));
+                System.Console.WriteLine(stream.toString());
+                stream.close();
+            }
+        }
+
     }
 }

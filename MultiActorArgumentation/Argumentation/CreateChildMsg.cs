f16c844ce0e08afd4f41679e1113d05d5412edc7
Krysol <Krysol11111@gmail.com>
2019-01-08 20:27:56 +0100

    basic tree building

--- MultiActorArgumentation/Argumentation/CreateChildMsg.cs
+++ MultiActorArgumentation/Argumentation/CreateChildMsg.cs
@@ -1,4 +1,7 @@
 ﻿
+using Akka.Actor;
+using System.Collections.Generic;
+
 namespace MultiActorArgumentation.Argumentation
 {
     public class CreateChildMsg
@@ -35,5 +38,52 @@ namespace MultiActorArgumentation.Argumentation
         }
 
         public string ArgumentationResult { get; private set; }
+    }
+
+    public class RelatedArgumentsQueryMsg
+    {
+        public RelatedArgumentsQueryMsg(int argument, IActorRef querySender)
+        {
+            BlacklistedArguments = new List<int>();
+            BlacklistedArguments.Add(argument);
+            QuerySender = querySender;
+        }
+
+        public void AppendArgument(int argument)
+        {
+            BlacklistedArguments.Add(argument);
+        }
+
+        public List<int> BlacklistedArguments { get; private set; }
+        public IActorRef QuerySender { get; private set; }
+    }
+
+    public class RelatedArgumentsDefenderResponseMsg
+    {
+        public RelatedArgumentsDefenderResponseMsg(List<int> argsList)
+        {
+            RelatedArguments = argsList;
+        }
+        public List<int> RelatedArguments { get; private set; }
+    }
+
+    public class RelatedArgumentsProsecutorResponseMsg
+    {
+        public RelatedArgumentsProsecutorResponseMsg(List<int> argsList)
+        {
+            RelatedArguments = argsList;
+        }
+        public List<int> RelatedArguments { get; private set; }
+    }
+
+    public class NodeResultMsg
+    {
+        public NodeResultMsg(int argument, bool active = true)
+        {
+            Argument = argument;
+            Active = active;
+        }
+        public int Argument { get; private set; }
+        public bool Active { get; private set; }
     }
 }

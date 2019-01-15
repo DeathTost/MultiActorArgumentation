38290497a422942f2bbb58c771664175f0b4b71c
DeathTost <deathtost@gmail.com>
2019-01-15 16:36:01 +0100

    Enable scheduled kill command

--- MultiActorArgumentation/Argumentation/JudgeActor.cs
+++ MultiActorArgumentation/Argumentation/JudgeActor.cs
@@ -102,7 +102,7 @@ namespace MultiActorArgumentation.Argumentation
                 {
                     Console.WriteLine("I killed a child and I liked it");
                     //x.QuerySender.Tell(Kill.Instance);
-                    //Context.System.Scheduler.ScheduleOnce(TimeSpan.FromSeconds(100), x.QuerySender, Kill.Instance);
+                    Context.System.Scheduler.ScheduleOnce(TimeSpan.FromSeconds(100), x.QuerySender, Kill.Instance);
                     KilledAChild = true;
                     return;
                 }
